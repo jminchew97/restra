efaf89dd2f538c96cb95cdb6387521b32d88b6c9
Joshua Minchew <jminchew97@gmail.com>
2023-06-28 11:06:17 -0500

    changed serials to uuid in migration file

--- src/main/resources/migrations/V1__Initialize_database.sql
+++ src/main/resources/migrations/V1__Initialize_database.sql
@@ -1,29 +1,23 @@
 CREATE TABLE restaurants
 (
-    id         SERIAL PRIMARY KEY,
+    id         UUID PRIMARY KEY,
     name       varchar,
     address    varchar,
     food_type  varchar,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
-INSERT INTO restaurants (name, address,food_type)
-VALUES ('Best Dim Sum', '123 da street', 'dim sum');
-
-INSERT INTO restaurants (name, address,food_type)
-VALUES ('Burger Palace', '123 westore ave', 'american');
 
 CREATE TABLE menus
 (
-    id         SERIAL PRIMARY KEY,
+    id         UUID PRIMARY KEY,
     name       varchar,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
-
 CREATE TYPE item_type AS ENUM ('DESSERT', 'DRINK', 'ENTREE', 'APPETIZER');
 CREATE TABLE items
 (
-    id          SERIAL PRIMARY KEY,
+    id          UUID PRIMARY KEY,
     name        varchar not null,
     price       integer,
     description varchar,
@@ -31,10 +25,9 @@ CREATE TABLE items
     created_at  timestamptz default current_timestamp
 );
 
-
 --Create and assign foreign keys to menu and item table
 ALTER TABLE menus
-    ADD COLUMN restaurant_id SERIAL;
+    ADD COLUMN restaurant_id UUID;
 
 ALTER TABLE menus
     ADD CONSTRAINT menu_constraint_restaurant_id
@@ -42,15 +35,13 @@ ALTER TABLE menus
             REFERENCES restaurants (id);
 
 ALTER TABLE items
-    ADD COLUMN menu_id SERIAL;
+    ADD COLUMN menu_id UUID;
 
 ALTER TABLE items
-    ADD COLUMN restaurant_id SERIAL;
+    ADD COLUMN restaurant_id UUID;
+
 ALTER TABLE items
     ADD CONSTRAINT item_constraint_menu_id
         FOREIGN KEY (menu_id)
             REFERENCES menus (id);
 
-
-INSERT INTO menus (restaurant_id, name)
-VALUES (1,'test');
